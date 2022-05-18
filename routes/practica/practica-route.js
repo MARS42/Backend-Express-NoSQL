116c4221dcd7c47c4221655bc7740b2afea1543d
Robert Mtz <msjred42@gmail.com>
2022-05-17 22:47:17 -0500

    Agregado ruta método delete

--- routes/practica/practica-route.js
+++ routes/practica/practica-route.js
@@ -48,9 +48,27 @@ router.get('/practica/alumnos/:id', async (req, res) => {
 
     const result = await model.findById(id);
 
+    if(result === null){
+        res.json({
+            ok: true,
+            msg: 'Sin resultados'
+        });
+        return;
+    }
+
     res.json({
         ok: true,
         data: result
+    });
+});
+
+router.delete('/practica/alumnos/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    const del = await model.findByIdAndDelete(id);
+    res.json({
+        ok: true,
+        msg: 'Alumno eliminado',
+        data: del
     });
 });
 
