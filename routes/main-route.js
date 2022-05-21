ca19eed1d128663ca8ae28e33db3d304747f484e
Robert Mtz <msjred42@gmail.com>
2022-05-20 23:24:10 -0500

    Agregada ruta funcional en index rutas

--- routes/main-route.js
+++ routes/main-route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('./user/user-route'));
-app.use(require('./practica/practica-route'))
+app.use(require('./practica/practica-route'));
+app.use(require('./practica/ferreteria-route'));
 
 module.exports = app;
